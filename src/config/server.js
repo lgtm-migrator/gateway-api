a0835d62f2da4af654540ac5919ba44a65b9980e
Diego Frison <diego.frison@paconsulting.com>
2020-05-01 15:35:45 +0100

    Moved counter & project routes to corresponding packages

--- src/config/server.js
+++ src/config/server.js
@@ -12,7 +12,6 @@ import cookieParser from "cookie-parser";
 
 import { connectToDatabase } from "./db"
 import { initialiseAuthentication } from "../resources/auth";
-import { Data } from '../resources/tool/data.model';
 
 require('dotenv').config();
 
@@ -39,55 +38,29 @@ app.use(passport.session());
 app.use('/api', router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use('/api/auth', require('../resources/auth/auth.route'));
+
 app.use('/api/v1/users', require('../resources/user/user.route'));
 app.use('/api/v1/messages', require('../resources/message/message.route'));
 app.use('/api/v1/reviews', require('../resources/tool/review.route'));
 app.use('/api/v1/tools', require('../resources/tool/tool.route'));
-
+app.use('/api/v1/accounts', require('../resources/account/account.route'));
 app.use('/api/v1/search/filter', require('../resources/search/filter.route'));
 
 app.use('/api/search', require('../resources/search/search.router'));
 app.use('/api/dataset', require('../resources/dataset/dataset.route'));
-
-app.use('/api/v1/accounts', require('../resources/account/account.route'));
 
 app.use('/api/stats', require('../resources/stats/stats.router'));
 
 app.use('/api/person', require('../resources/person/person.route'));
 
 app.use('/api/mytools', require('../resources/mytools/mytools.route'));
+app.use('/api/project', require('../resources/project/project.route'));
+app.use('/api/counter', require('../resources/tool/counter.route'));
 
 app.use('/api/auth/register', require('../resources/user/user.register.route'));
-app.use('/api/auth', require('../resources/auth/auth.route'));
 
 initialiseAuthentication(app);
-
-router.post(
-  '/counter/update',
-  async(req, res) => {
-    const {id, counter} = req.body;
-    Data.findOneAndUpdate({id: id},
-         {counter: counter}, (err) => {
-          if (err) return res.json({ success: false, error: err });
-          return res.json({ success: true });
-        });
-  });
-
-/**
- * {get} /project​/:project​ID Project
- * 
- * Return the details on the tool based on the tool ID.
- */
-router.get('/project/:projectID', async (req, res) => {
-  var q = Data.aggregate([
-    { $match: { $and: [{ id: parseInt(req.params.projectID) }] } },
-    { $lookup: { from: "tools", localField: "authors", foreignField: "id", as: "persons" } }
-  ]);
-  q.exec((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
