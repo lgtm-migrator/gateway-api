f33ec3e597f88a80eaacb3460671abb3634c179f
Crippy <krismarks@rocketmail.com>
2020-05-11 15:03:36 +0100

    Sendgrid implementation completed

--- src/resources/dataset/dataset.route.js
+++ src/resources/dataset/dataset.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import axios from 'axios';
 import { UserModel } from '../user/user.model';
-import {sgMail} from '@sendgrid/mail';
+const sgMail = require('@sendgrid/mail');
 
 const router = express.Router()
 
@@ -40,36 +40,36 @@ router.get('/:id', async (req, res) => {
       contactNumber,
       title,
       userId
-    } = req;
+    } = req.body;
 
     const sendSuccess = {type: 'success', message: 'Done! Your request for data access has been sent to the data custodian.'};
 
     try {
-      const user = await UserModel.findById({id: userId});
-      debugger;
-      console.log(user);
+      const user = await UserModel.findOne({id: userId});
+
       if (!user) {
         return res
           .status(400)
-          .json({ message: { type:'error', message: 'User not found' } });
+          .json({ message: { type:'danger', message: 'User not found' } });
       }
 
       const msg = {
         to: user.email,
-        from: 'support@healthdatagateway.org',
-        subject: `Enquires for ${tile} dataset healthdatagateway.org`,
+        from: 'tony.espley@paconsulting.com',
+        subject: `Enquires for ${title} dataset healthdatagateway.org`,
         text: 'and easy to do anywhere, even with Node.js',
         html: `Thank you for enquiring about access to the ${title} dataset through the Health Data Research UK Innovation Gateway. The Data Custodian for this dataset has been notified and they will contact you directly in due course.<br />
+
         In order to facilitate the next stage of the request process, please make yourself aware of the technical data terminology used by the NHS Data Dictionary on the following link: <a href="https://www.datadictionary.nhs.uk/">https://www.datadictionary.nhs.uk/</a><br />
+
         Please reply to this email, if you would like to provide feedback to the Data Enquiry process facilitated by the Health Data Research Innovation Gateway - <a href="mailto:support@healthdatagateway.org(opens in new tab)">support@healthdatagateway.org(opens in new tab)</a>`,
       };
-
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       await sgMail.send(msg);      
       res.json({message: {...sendSuccess}});
     }
     catch(error) {
-      console.error(error);
-      res.status(500).json({message: {type: 'error', message: 'Something went wrong and your request could not be sent.'}});
+      res.status(500).json({message: {type: 'danger', message: 'Something went wrong and your request could not be sent.'}});
     }
   });
 
