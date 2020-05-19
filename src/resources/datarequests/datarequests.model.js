26dec917aa1e2fa20ba620ca26d9c73818998675
Chris Marks <chris.marks@paconsulting.com>
2020-05-19 12:19:40 +0100

    Data request access updated to contain custodian email, new comments and tested locallyto get details. Moment js added for date formatting

--- src/resources/datarequests/datarequests.model.js
+++ src/resources/datarequests/datarequests.model.js
@@ -5,6 +5,9 @@ const DataRequestSchema = new Schema({
   dataSetId: String,
   userId: Number,
   timeStamp: Date
+},
+{ 
+  collection: 'data_requests',
 });
 
 export const DataRequestModel = model('DataRequest', DataRequestSchema)
