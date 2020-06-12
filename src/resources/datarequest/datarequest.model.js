d6c1f7556a9a393452eafa7a6c3fd4850888c964
Chris Marks <chris.marks@paconsulting.com>
2020-06-12 15:46:58 +0100

    Updated questionAnswers

--- src/resources/datarequest/datarequest.model.js
+++ src/resources/datarequest/datarequest.model.js
@@ -1,10 +1,13 @@
 import { model, Schema } from 'mongoose';
 
 const DataRequestSchema = new Schema({
-  id: String,
   version: Number,
   userId: Number,
   dataSetId: String,
+  applicationStatus: {
+    type: String,
+    default: "inProgress"
+  },
   jsonSchema: {
     type: String,
     default: "{}"
