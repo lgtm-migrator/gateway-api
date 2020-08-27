748214e9904b3e3886e8a99e63a9ec661105e369
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2020-08-27 15:33:44 +0100

    Completed backend work with exception of data persistence

--- src/resources/publisher/publisher.controller.js
+++ src/resources/publisher/publisher.controller.js
@@ -24,11 +24,16 @@ module.exports = {
     getPublisherDatasets: async(req, res) => {
         try {
             // 1. Get the datasets for the publisher from the database
-            const datasets = await Data.find({ type: 'dataset', 'datasetfields.publisher': req.params.id }).select('datasetid name description datasetfields.abstract -_id');
+            let datasets = await Data.find({ type: 'dataset', 'datasetfields.publisher': req.params.id }).select('datasetid name description datasetfields.abstract _id datasetfields.publisher');
             if(!datasets) {
                 return res.status(404).json({ success: false });
             }
-            // 2. Return datasets
+            // 2. Map datasets to flatten datasetfields nested object
+            datasets = datasets.map(dataset => {
+				let { _id, datasetid, name, description, datasetfields: { abstract, publisher }} = dataset;
+				return { _id, datasetid, name, description, abstract, publisher };
+			});
+            // 3. Return publisher datasets
             return res.status(200).json({ success: true, datasets });
         } catch (err) {
             console.error(err.message);
