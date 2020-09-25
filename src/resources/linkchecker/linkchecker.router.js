834c3af8c9004702ea702dfcdaa7b8179577d4e7
Ciara <ciara.ward@paconsulting.com>
2020-09-25 09:36:56 +0100

    IG-672 application link checker

--- src/resources/linkchecker/linkchecker.router.js
+++ src/resources/linkchecker/linkchecker.router.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import request from 'request'
 import { getObjectResult } from './linkchecker.repository';
+// import _ from 'lodash';
+var async = require("async");
+import eachOf from 'async/eachOf';
+const axios = require('axios');
+
 
 const router = express.Router(); 
 
@@ -37,54 +42,155 @@ router.get('/', async (req, res) => {
 
     getErrorLinks(result);
     
-    // getErrorLinks(result).then(console.log);
-
-
     // result.forEach(async (item) => { 
     //     await getErrorLinks(item);
     // })
 
 
  
-//CHECK URLS FOR RESPONSE 
+//CHECK URLS FOR RESPONSE
 
 async function getErrorLinks(result) {
     console.log('in function')
+   let errorLinks =  [];
     links = [];
         
+    console.log('errorLinks: ' + JSON.stringify(errorLinks))
         // await result.forEach(async (item) => { 
         // new Promise((resolve, reject) => {
 
-        result.forEach((item) => { 
 
-             console.log('in await')
-            if(item.description){
-                console.log('in if')
+    //TRY THIS ASYNC FOR EACH OF -- still doing everything before requests
+            async.forEachOf(result, function (item, index, callback) {
 
-                item.description.forEach((link) => {
-                    console.log('in for each')
+                if (item.description) {
+                    console.log('in if')
 
+                    item.description.forEach((link) => {
+                        console.log('in for each')
+    
+                        // console.log('item.persons[0].id: ' + item.persons[0].id)
+    
+                    // request(link, async function(error, response) {     
+                    //     console.log('in request')
+       
+                    //         if (error || response.statusCode != 200) {
+                    //             links.push(link)
+    
+                    //             console.log('link in error: ' + links)
+                    //         } 
+    
+                    //         console.log('in end request')
+    
+                    //     });
 
-                request(link, async function(error, response) {     
-                    console.log('in request')
-
-                        if (!error && response && response.statusCode == 200) {
-                            console.log('success')
-                        } else {
-                            links.push(link)
-                            console.log('link in error: ' + links)
-                            console.log('error') 
+                    (async () => {
+                        try {
+                          const response = await axios.get(link)
+                          console.log('response.data.url: ' + response.data.url);
+                          console.log('response.data.explanation: ' + response.data.explanation);
+                        } catch (error) {
+                          console.log('error.response.body ' + error.response.body);
                         }
-                        console.log('in end request')
-
+                      })();
+    
                     });
+                }
+              
+                // return callback(links); 
+                return callback(links); 
+                console.log('CALLBACK')
 
-                });
+               }, function (err) {
+                if (err) console.error(err.message);
 
-            }
-            console.log('in end of await')
+                console.log('well? ' + links);
+                // callback(err);
+               });
 
-        })
+
+
+//FETCH INSTEAD OF REQUEST
+        // result.forEach((item) => { 
+        //     console.log('item: ' + JSON.stringify(item))
+        //      console.log('in await')
+        //     if(item.description){
+        //         console.log('in if')
+
+        //         item.description.forEach((link) => {
+        //             console.log('in for each')
+        //             const fetch = require('node-fetch');
+
+        //             fetch(link)
+        //             .then(response => {
+        //             if (!response.ok) {
+        //                 links.push(link)
+        //                 console.log('error link - ' + link)
+        //                 console.log('link in error: ' + links)
+
+        //                 throw new Error(`HTTP error! status: ${response.status}`);
+        //             } else {
+        //                 console.log('success - ' + link )
+        //                 // return response.blob();
+        //             }
+        //             })
+
+
+//REQUEST 
+        //  async function doRequests(link) {
+                    
+                    // request(link, async function(error, response) {     
+                    // console.log('in request')
+
+                    //     // if (!error && response && response.statusCode == 200) {
+                    //     //     console.log('success')
+                    //     // } else {
+                    //     //     links.push(link)
+                    //     //     console.log('link in error: ' + links)
+                    //     //     console.log('error') 
+                    //     // }
+
+                    //     if (error || response.statusCode != 200) {
+                    //         links.push(link)
+
+                    //         item.persons.map((person) => {
+                    //             console.log('person: ' + JSON.stringify(person))
+                    //             errorLinks.push(
+                    //                 {
+                    //                     'personId' : person.id,
+                    //                     'link': link,
+                    //                     'type': item.type,
+                    //                     'name': item.name  
+                    //                 }
+                    //             )
+                    //         })
+                    //         console.log('link in error: ' + links)
+                    //         // _.groupBy(errorLinks, 'personId') 
+                    //         // errorLinks = errorLinks.reduce(())
+                    //         console.log('errorLinks in error: ' + JSON.stringify(errorLinks))
+
+
+                    //         console.log('error - ' + error + ' : ' + response.statusCode)
+                    //     } 
+
+                    //     console.log('in end request')
+
+                    // });
+
+                // }
+
+
+
+
+                // });
+
+        //     }
+        //     console.log('in end of await')
+
+        // })
+
+
+
             // resolve();
                 // });
 
