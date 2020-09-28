f9077d03971bac2eff58050bd87c0edaf6f770a7
Ciara <ciara.ward@paconsulting.com>
2020-09-28 11:09:05 +0100

    IG-672 link checker

--- src/resources/linkchecker/linkchecker.router.js
+++ src/resources/linkchecker/linkchecker.router.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import request from 'request'
 import { getObjectResult } from './linkchecker.repository';
-// import _ from 'lodash';
+import { getUserByUserId } from '../user/user.repository';
+import { UserModel } from '../user/user.model';
+import emailGenerator from '../utilities/emailGenerator.util';
+const hdrukEmail = `enquiry@healthdatagateway.org`;
+
 var async = require("async");
-import eachOf from 'async/eachOf';
 const axios = require('axios');
 
 
@@ -11,6 +14,9 @@ const router = express.Router();
 
 // let links = [];
 let links;
+let errorLinks;
+let responses; 
+let users = [];
 
 router.get('/', async (req, res) => {
 
@@ -42,266 +48,166 @@ router.get('/', async (req, res) => {
 
     getErrorLinks(result);
     
-    // result.forEach(async (item) => { 
-    //     await getErrorLinks(item);
-    // })
-
-
  
 //CHECK URLS FOR RESPONSE
 
 async function getErrorLinks(result) {
     console.log('in function')
-   let errorLinks =  [];
+   errorLinks =  [];
     links = [];
-        
-    console.log('errorLinks: ' + JSON.stringify(errorLinks))
-        // await result.forEach(async (item) => { 
-        // new Promise((resolve, reject) => {
 
 
     //TRY THIS ASYNC FOR EACH OF -- still doing everything before requests
-            async.forEachOf(result, function (item, index, callback) {
+            async.forEachOf(result, async function (item, index, callback) {
 
                 if (item.description) {
-                    console.log('in if')
+                 
+
+//START ONE
 
                     item.description.forEach((link) => {
-                        console.log('in for each')
-    
-                        // console.log('item.persons[0].id: ' + item.persons[0].id)
-    
-                    // request(link, async function(error, response) {     
-                    //     console.log('in request')
-       
-                    //         if (error || response.statusCode != 200) {
-                    //             links.push(link)
-    
-                    //             console.log('link in error: ' + links)
-                    //         } 
-    
-                    //         console.log('in end request')
-    
-                    //     });
 
-                    (async () => {
+                      main(link).then(console.log('THEN'))
+
+                    async function main(link) {
                         try {
-                          const response = await axios.get(link)
-                          console.log('response.data.url: ' + response.data.url);
-                          console.log('response.data.explanation: ' + response.data.explanation);
-                        } catch (error) {
-                          console.log('error.response.body ' + error.response.body);
-                        }
-                      })();
-    
+                            const response = await axios.get(link)
+                            console.log('response: ' + link + ' - ' + response);
+                          } catch (error) {
+                            links.push(link)
+
+                            item.persons.map((person) => {
+                                // console.log('person: ' + JSON.stringify(person))
+                                errorLinks.push(
+                                    {
+                                        'personId' : person.id,
+                                        'link': link,
+                                        'type': item.type,
+                                        'id': item.id,
+                                        'name': item.name  
+                                    }
+                                )
+                            })
+
+                            console.log('error: ' + link + ' - ' + error);
+                            console.log('links are: ' + links)
+                            console.log('error links are: ' + JSON.stringify(errorLinks))
+                          }
+                    }
+
+
                     });
+
                 }
               
-                // return callback(links); 
                 return callback(links); 
-                console.log('CALLBACK')
 
                }, function (err) {
                 if (err) console.error(err.message);
-
-                console.log('well? ' + links);
-                // callback(err);
                });
 
 
-
-//FETCH INSTEAD OF REQUEST
-        // result.forEach((item) => { 
-        //     console.log('item: ' + JSON.stringify(item))
-        //      console.log('in await')
-        //     if(item.description){
-        //         console.log('in if')
-
-        //         item.description.forEach((link) => {
-        //             console.log('in for each')
-        //             const fetch = require('node-fetch');
-
-        //             fetch(link)
-        //             .then(response => {
-        //             if (!response.ok) {
-        //                 links.push(link)
-        //                 console.log('error link - ' + link)
-        //                 console.log('link in error: ' + links)
-
-        //                 throw new Error(`HTTP error! status: ${response.status}`);
-        //             } else {
-        //                 console.log('success - ' + link )
-        //                 // return response.blob();
-        //             }
-        //             })
-
-
-//REQUEST 
-        //  async function doRequests(link) {
-                    
-                    // request(link, async function(error, response) {     
-                    // console.log('in request')
-
-                    //     // if (!error && response && response.statusCode == 200) {
-                    //     //     console.log('success')
-                    //     // } else {
-                    //     //     links.push(link)
-                    //     //     console.log('link in error: ' + links)
-                    //     //     console.log('error') 
-                    //     // }
-
-                    //     if (error || response.statusCode != 200) {
-                    //         links.push(link)
-
-                    //         item.persons.map((person) => {
-                    //             console.log('person: ' + JSON.stringify(person))
-                    //             errorLinks.push(
-                    //                 {
-                    //                     'personId' : person.id,
-                    //                     'link': link,
-                    //                     'type': item.type,
-                    //                     'name': item.name  
-                    //                 }
-                    //             )
-                    //         })
-                    //         console.log('link in error: ' + links)
-                    //         // _.groupBy(errorLinks, 'personId') 
-                    //         // errorLinks = errorLinks.reduce(())
-                    //         console.log('errorLinks in error: ' + JSON.stringify(errorLinks))
-
-
-                    //         console.log('error - ' + error + ' : ' + response.statusCode)
-                    //     } 
-
-                    //     console.log('in end request')
-
-                    // });
-
-                // }
-
-
-
-
-                // });
-
-        //     }
-        //     console.log('in end of await')
-
-        // })
-
-
-
-            // resolve();
-                // });
-
-        // console.log('in end try')
-
-        // console.log('links at end of try: ' + links)
-
-    // } catch(err) {
-    //     console.log('ERROR')
-    // }
-
-    console.log('in end function')
-
     console.log('links at end of function: ' + links)
-
 
 }
 
 
 
-// async function getErrorLinks(result) {
-// // async function getErrorLinks(item) {
-
-//     // let links = [];
-//     // let templinks = [];
 
 
-//     // let promise = new Promise((resolve, reject) => {
+//GET EMAILS FROM USERS COLLECTION 
+
+    //REDO
+    // users = await getEmails();
+
+    async function getEmails (){
+
+        // let tempUsers = [{"personId":947228017269611,"link":"https://en.wikipedia.org/wiki/Bob_Rossbhdu","type":"tool","name":"787"}, {"personId":46035149615760184,"link":"https://en.wikipedia.org/wiki/Bob_Rossbhdu","type":"tool","name":"787"}]
+        let tempUsers = [{"personId":947228017269611,"link":"http://latest.healthdatagateway.org/","type":"tool","id":701594274314441,"name":"787"},{"personId":46035149615760184,"link":"https://en.wikipedia.org/wiki/Bob_Rossbhdu","type":"tool","id":701594274314441,"name":"787"}]
+        await Promise.all( tempUsers.map( async (user) => {
+            let tempUser = await getUserByUserId(user.personId);
+            users.push(tempUser)
+        }))
+
+        return users;
+    }
 
 
-//        await result.forEach(async (item) => { 
+//SEND EMAILS ABOUT LINKS
+    // sendEmailNotifications(errorLinks, users);
+    let resources = [{"personId":947228017269611,"link":"http://latest.healthdatagateway.org/","type":"tool","id":701594274314441,"name":"787"},{"personId":46035149615760184,"link":"https://en.wikipedia.org/wiki/Bob_Rossbhdu","type":"tool","id":701594274314441,"name":"787"}];
+    // sendEmailNotifications(resources, users);
 
-//             // if(item.persons[0]){
-//             //     console.log('person: ' + JSON.stringify(item.persons[0].id))
-//             // }
+    resources.map(async (resource) => {
+        users.map(async (user) => {
+            console.log('user id: ' +  user.id)
+            console.log('person id: ' +  resource.personId)
+            if (user.id === resource.personId){
+                //REDO
+                // sendEmailNotifications(resource, user);
+            }
 
+        })
+        // sendEmailNotifications(resource, users);
+    })
 
-//             // if(item.link){
-//             //     request(item.link, function(error, response) {  
+    async function sendEmailNotifications(resource, user) {
+        console.log('resource:  ' + JSON.stringify(resource))
+        console.log('user:  ' + user)
 
-//             //         if (!error && response && response.statusCode == 200) {
-//             //             console.log('NEXT: ') 
-//             //             console.log('item.link: ' + item.link) 
-//             //             console.log('success')
-//             //         } else {
-//             //             // links.push(item.link)
-//             //             console.log('NEXT: ') 
-//             //             console.log('item.link: ' + item.link) 
-//             //             console.log('error')
-//             //         }
-//             //     });
-//             // }
+        let subject;
+        let html;
+        let emailRecipients;
 
-
-//             if(item.description){
-//                 item.description.forEach((link) => {
-//                     // links.push(link)
-
-//                     request(link, function(error, response) {        
-//                         if (!error && response && response.statusCode == 200) {
-//                             // console.log('NEXT: ') 
-//                             // console.log('link: ' + link) 
-//                             // console.log('success')
-//                         } else {
-//                             links.push(link)
-//                             // console.log('links: ' + links)
-//                             // console.log('NEXT: ') 
-//                             // console.log('link: ' + link) 
-//                             // console.log('error')
-//                         }
-//                       });
-//                 });
-//             }
-            
-//             // console.log('links: ' + links)
-
-//             // if(item.resultsInsights){
-//             //     item.resultsInsights.forEach((link) => {
-//             //        request(link, async function(error, response) {        
-//             //             if (!error && response && response.statusCode == 200) {
-//             //                 // console.log('NEXT: ') 
-//             //                 // console.log('link: ' + link) 
-//             //                 console.log('success')
-//             //             } else {
-//             //                 links.push(link)
-//             //                 console.log('links: ' + links)
-
-//             //                 // console.log('NEXT: ') 
-//             //                 // console.log('link: ' + link) 
-//             //                 // console.log('error')
-//             //             }
-//             //           });
-//             //     });
-//             // }
-
-//     });
-
-
-// // });
-
-// //  console.log('links: ' + links)
-
-// }
-
+        // let emailRecipients = [{'email':user.email}]
+        // 1. Generate URL for linking collection in email
+        const resourceLink = process.env.homeURL + '/' + resource.type + '/' + resource.id;
+        
+        // emailRecipients = user;
+        emailRecipients = [{"_id":"5e6f984a0a7300dc8f6fb195","firstname":"Ciara","lastname":"Ward","email":"ciara.ward@paconsulting.com","role":"Admin","tool":[{"emailNotifications":true}]}];
+        console.log('WHAT?! ' + JSON.stringify(emailRecipients))
     
+        // 2. Build email body
+            subject = 'Links in ${NAME} require updating.' 
+            html = `${user.firstname} ${user.lastname}, your ${resource.type} ${resource.name} contains stale links, please update these here: ${resourceLink}`
+    
+        // // 3. Query Db for all admins or authors of the collection who have opted in to email updates
+        var q = UserModel.aggregate([
+          // Find all users who are admins or authors of this collection
+        //   { $match: { $or: [{ role: 'Admin' }, { id: { $in: collections.authors } }] } },
+        { $match: { 'tool.id': user.id } },
+          // Perform lookup to check opt in/out flag in tools schema
+        //   { $lookup: { from: 'tools', localField: 'id', foreignField: 'id', as: 'tool' } },
+          // Filter out any user who has opted out of email notifications
+        //   { $match: { 'tool.emailNotifications': true } },
+          // Reduce response payload size to required fields
+        //   { $project: { _id: 1, firstname: 1, lastname: 1, email: 1, role: 1, 'tool.emailNotifications': 1 } }
+        ]); 
+    
+        // // 4. Use the returned array of email recipients to generate and send emails with SendGrid
+        q.exec((err, emailRecipients) => {
+            console.log('emailRecipients in link checker: ' + emailRecipients)
+          if (err) {
+            return new Error({ success: false, error: err });
+          }
+          emailGenerator.sendEmail( 
+            emailRecipients,
+            `${hdrukEmail}`,
+            subject,
+            html
+          );
+        });
+      }
+
+     
 
     return res.json({
         success: true,
-        links: links,
+        links: links, 
+        users: users,
         result: result
     });
 });
+
 
 module.exports = router;
