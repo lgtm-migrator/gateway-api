95f9334568ab7ae942440303823d819b487ba698
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2020-10-02 11:18:52 +0100

    Continued update workflow endpoint

--- src/resources/workflow/workflow.controller.js
+++ src/resources/workflow/workflow.controller.js
@@ -151,71 +151,80 @@ module.exports = {
 	// PUT api/v1/workflows/:id
 	updateWorkflow: async (req, res) => {
 		try {
-			// 1. Look at the payload for the publisher passed
-			let workflow = new WorkflowModel({ test: 'lol' });
-
-			workflow.save();
-
-			// 2. Check if the user is a manager of that team
-
-			// 3. Create new workflow model
-
-			// 4. Return workflow payload
-
-			// // 1. Get the workflow from the database including the team members to check authorisation
-			// const workflow = await WorkflowModel.findOne({
-			// 	_id: req.params.id,
-			// }).populate({
-			// 	path: 'publisher steps.reviewers',
-			// 	select: 'team',
-			// 	populate: {
-			// 		path: 'team',
-			// 		select: 'members -_id',
-			// 	},
-			// });
-			// if (!workflow) {
-			// 	return res.status(404).json({ success: false });
-			// }
-			// // 2. Check the requesting user is a member of the team
-			// let { _id: userId } = req.user;
-			// let members = [],
-			// 	authorised = false;
-			// if (_.has(workflow.toObject(), 'publisher.team')) {
-			// 	({
-			// 		publisher: {
-			// 			team: { members },
-			// 		},
-			// 	} = workflow);
-			// }
-			// if (!_.isEmpty(members)) {
-			// 	authorised = members.some(
-			// 		(el) => el.memberid.toString() === userId.toString()
-			// 	);
-			// }
-			// // 3. If not return unauthorised
-			// if (!authorised) {
-			// 	return res.status(401).json({ success: false });
-			// }
-			// // 4. Return workflow
-			// let {
-			// 	active,
-			// 	_id,
-			// 	id,
-			// 	workflowName,
-			// 	version,
-			// 	steps,
-			// } = workflow.toObject();
-			// return res
-			// 	.status(200)
-			// 	.json({
-			// 		success: true,
-			// 		workflow,
-			// 	});
+			const { _id: userId } = req.user;
+			const { id: workflowId } = req.params;
+			// 1. Look up workflow
+			let workflow = await WorkflowModel.findOne({
+				_id: req.params.id,
+			}).populate({
+				path: 'publisher steps.reviewers',
+				select: 'team',
+				populate: {
+					path: 'team',
+					select: 'members -_id',
+				},
+			});
+			if (!workflow) {
+				return res.status(404).json({ success: false });
+			}
+			// 2. Check the requesting user is a manager of the custodian team
+			let authorised = module.exports.checkWorkflowPermissions(
+				teamRoles.MANAGER,
+				workflow.publisher.toObject(),
+				userId
+			);
+			// 3. Refuse access if not authorised
+			if (!authorised) {
+				return res
+					.status(401)
+					.json({ status: 'failure', message: 'Unauthorised' });
+			}
+			// 4. Ensure there are no in-review DARs with this workflow
+			const applications = await DataRequestModel.countDocuments({ workflowId, 'applicationStatus':'inReview' });
+			if(applications > 0) {
+				return res.status(400).json({
+					success: false,
+					message: 'A workflow which is attached to applications currently in review cannot be edited',
+				});
+			}
+			// 5. Edit workflow
+			const { workflowName = '', steps = [] } = req.body;
+			let isDirty = false;
+			// Check if workflow name updated
+			if(!_.isEmpty(workflowName)) {
+				workflow.workflowName = workflowName;
+				isDirty = true;
+			} // Check if steps updated
+			if(!_.isEmpty(steps)) {
+				workflow.steps = steps;
+				isDirty = true;
+			} // Perform save if 
+			if(isDirty) {
+				WorkflowModel.deleteOne({ _id: workflowId }, function (err) {
+					if (err) {
+						console.error(err);
+						return res.status(400).json({
+							success: false,
+							message: 'An error occurred editing the workflow',
+						});
+					} else {
+						// 7. Return workflow payload
+						return res.status(204).json({
+							success: true
+						});
+					}
+				});
+			} else {
+				return res.status(200).json({
+					success: true,
+					workflow
+				});
+			}
 		} catch (err) {
 			console.error(err.message);
 			return res.status(500).json({
 				success: false,
-				message: 'An error occurred updating the workflow',
+				message: 'An error occurred editing the workflow',
 			});
 		}
 	},
