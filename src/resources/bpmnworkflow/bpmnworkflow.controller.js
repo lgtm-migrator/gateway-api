36837dc78fae76f18d7da91479b8fbb3be12f376
Alex Power <alex.power@paconsulting.com>
2020-10-05 11:50:37 +0100

    Defined the camunda endpoints in bpmnworkflow controller

--- src/resources/bpmnworkflow/bpmnworkflow.controller.js
+++ src/resources/bpmnworkflow/bpmnworkflow.controller.js
@@ -6,10 +6,17 @@ axiosRetry(axios, { retries: 3, retryDelay: () => {
     return 3000;
   }});
 
+const bpmnBaseUrl = process.env.BPMNBASEURL;
+
 module.exports = {
-    postCreateProcess: async (bmpContext) => {
+    //Generic Get Task Process Endpoints
+    getProcess: async (businessKey) => {
+        return await axios.get(`${bpmnBaseUrl}/engine-rest/task?processInstanceBusinessKey=${businessKey.toString()}`);
+    },
+    //Simple Workflow Endpoints
+    postCreateProcess: async (bpmContext) => {
         // Create Axios requet to start Camunda process
-        let { applicationStatus, dateSubmitted, publisher, actioner, businessKey } = bmpContext;
+        let { applicationStatus, dateSubmitted, publisher, actioner, businessKey } = bpmContext;
         let data = {
             "variables": {
                 "applicationStatus": {
@@ -31,14 +38,14 @@ module.exports = {
             },
             "businessKey": businessKey.toString()
         }
-        await axios.post(`${process.env.BPMNBASEURL}/engine-rest/process-definition/key/GatewayWorkflowSimple/start`, data)
+        await axios.post(`${bpmnBaseUrl}/engine-rest/process-definition/key/GatewayWorkflowSimple/start`, data)
             .catch((err) => { 
                 console.error(err);
             });
     },
-    postUpdateProcess: async (bmpContext) => {
+    postUpdateProcess: async (bpmContext) => {
         // Create Axios requet to start Camunda process
-        let { taskId, applicationStatus, dateSubmitted, publisher, actioner, archived } = bmpContext;
+        let { taskId, applicationStatus, dateSubmitted, publisher, actioner, archived } = bpmContext;
         let data = {
             "variables": {
                 "applicationStatus": {
@@ -63,12 +70,109 @@ module.exports = {
                 }
             }
         }
-        await axios.post(`${process.env.BPMNBASEURL}/engine-rest/task/${taskId}/complete`, data)
+        await axios.post(`${bpmnBaseUrl}/engine-rest/task/${taskId}/complete`, data)
             .catch((err) => { 
                 console.error(err);
             });
     },
-    getProcess: async (businessKey) => {
-        return await axios.get(`${process.env.BPMNBASEURL}/engine-rest/task?processInstanceBusinessKey=${businessKey.toString()}`);
+    //Complex Workflow Endpoints
+    postStartPreReview: async (bpmContext) => {
+        //Start pre-review process
+        let { applicationStatus, dateSubmitted, publisher, businessKey } = bpmContext;
+        let data = {
+            "variables": {
+                "applicationStatus": {
+                    "value": applicationStatus,
+                    "type": "String"
+                },
+                "dateSubmitted": {
+                    "value": dateSubmitted,
+                    "type": "String"
+                },
+                "publisher": {
+                    "value": publisher,
+                    "type": "String"
+                }
+            },
+            "businessKey": businessKey.toString()
+        }
+        await axios.post(`${bpmnBaseUrl}/engine-rest/process-definition/key/GatewayReviewWorkflowComplex/start`, data)
+            .catch((err) => {
+                console.error(err);
+            });
+    },
+    postStartManagerReview: async (bpmContext) => {
+        // Start manager-review process
+        let { applicationStatus, managerId, publisher, notifyManager } = bpmContext;
+        let data = {
+            "variables": {
+                "applicationStatus": {
+                    "value": applicationStatus,
+                    "type": "String"
+                },
+                "userId": {
+                    "value": managerId,
+                    "type": "String"
+                },
+                "publisher": {
+                    "value": publisher,
+                    "type": "String"
+                },
+                "notifyManager": {
+                    "value": notifyManager,
+                    "type": "String"
+                }
+            }
+        }
+        await axios.post(`${bpmnBaseUrl}/engine-rest/task/${taskId}/complete`, data)
+            .catch((err) => { 
+                console.error(err);
+            });
+    },
+    postManagerApproval: async (bpmContext) => {
+        // Manager has approved sectoin
+        let { applicationStatus, managerId, publisher } = bpmContext;
+        let data = {
+            "dataRequestStatus": applicationStatus,
+            "dataRequestManagerId": managerId,
+            "dataRequestPublisher": publisher,
+            "managerApproved": true
+        }
+        await axios.post(`${bpmnBaseUrl}/manager/completed/${businessKey}`)
+        .catch((err) => {
+            console.error(err);
+        })
+    },
+    postStartStepReview: async (bpmContext) => {
+        //Start Step-Review process
+        let { applicationStatus, userId, publisher, stepName, notifyReviewerSLA, reviewerList, businessKey } = bpmContext;
+        let data = {
+            "dataRequestStatus": applicationStatus,
+            "dataRequestUserId": userId,
+            "dataRequestPublisher": publisher,
+            "dataRequestStepName": stepName,
+            "notifyReviewerSLA": notifyReviewerSLA,
+            "reviewerList": reviewerList
+        }
+        await axios.post(`${bpmnBaseUrl}/complete/review/${businessKey}`, data)
+            .catch((err) => {
+                console.error(err);
+            });
+    },
+    postStartNextStep: async (bpmContext) => {
+        //Start Next-Step process
+        let { userId, publisher = "", stepName = "", notifyReviewerSLA = "", phaseApproved = false, reviewerList = [], businessKey } = bpmContext;
+        let data = {
+            "dataRequestUserId": userId,
+            "dataRequestPublisher": publisher,
+            "dataRequestStepName": stepName,
+            "notifyReviewerSLA": notifyReviewerSLA,
+            "phaseApproved": phaseApproved,
+            "reviewerList": reviewerList
+        }
+        await axios.post(`${bpmnBaseUrl}/reviewer/complete/${businessKey}`, data)
+        .catch((err) => {
+            console.error(err);
+        });
     }
 }
