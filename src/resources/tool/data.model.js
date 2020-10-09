c2ba474ea23f8a191cbec281674f6b6d81ddb570
Ciara <ciara.ward@paconsulting.com>
2020-10-09 13:27:18 +0100

    IG-403 data utility added to data model

--- src/resources/tool/data.model.js
+++ src/resources/tool/data.model.js
@@ -57,7 +57,7 @@ const DataSchema = new Schema(
     organisation: String,
     showOrganisation: {type: Boolean, default: false },
 
-    //dataset related fields
+    //dataset related fields 
     datasetid: String,
     datasetfields: {
         publisher: String,
@@ -77,6 +77,7 @@ const DataSchema = new Schema(
         periodicity: String,
         populationSize: String,
         metadataquality : {},
+        datautility : {},
         metadataschema : {},
         technicaldetails : [],
         versionLinks: [],
