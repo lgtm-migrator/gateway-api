04d3f71f5a3a4b33e0cc5f0797f339a099ca5a80
Paul McCafferty <Paul.McCafferty@PACONSULTING.COM>
2020-10-20 19:09:06 +0100

    Update to end session endpoint

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -102,20 +102,6 @@ async function logoutSource(ctx, form) {
 
   async function postLogoutSuccessSource(ctx) {
     // @param ctx - koa request context
-    const {
-      clientId, clientName, clientUri, initiateLoginUri, logoUri, policyUri, tosUri,
-    } = ctx.oidc.client || {}; // client is defined if the user chose to stay logged in with the OP
-    const display = clientName || clientId;
-    ctx.body = `<!DOCTYPE html>
-      <head>
-        <title>Sign-out Success</title>
-        <style>/* css and html classes omitted for brevity, see lib/helpers/defaults.js */</style>
-      </head>
-      <body>
-        <div>
-          <h1>Sign-out Success</h1>
-          <p>Your sign-out ${display ? `with ${display}` : ''} was successful.</p>
-        </div>
-      </body>
-      </html>`;
+    ctx.res.clearCookie('jwt');
+    ctx.res.status(200).redirect(process.env.homeURL+'/search?search=');
   }
