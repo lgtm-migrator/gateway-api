e651d1b4cd34b59f6023171af08b3a05e2e94413
Ciara <ciara.ward@paconsulting.com>
2020-10-21 18:16:31 +0100

    IG-767 Start of top datasets api

--- src/resources/stats/kpis.router.js
+++ src/resources/stats/kpis.router.js
@@ -235,6 +235,146 @@ router.get('', async (req, res) => {
         });
       
         return result;
+      break; 
+
+      case 'topdatasets':
+
+        let datasetIds = DataRequestModel.find(
+          {
+            // VALUES YOU ARE CHECKING MATCH SPECIFIED CRITERIA IE. WHERE
+            $and: [
+              {
+                $or: [
+                  {
+                    createdAt: {
+                      $gte: selectedMonthStart,
+                      $lt: selectedMonthEnd
+                    }
+                  },
+                  {
+                    timeStamp: {
+                      $gte: selectedMonthStart,
+                      $lt: selectedMonthEnd
+                    }
+                  }
+                ]
+              },
+              {
+                $or: [
+                  { applicationStatus: "submitted" },
+                  { applicationStatus: "approved" },
+                  { applicationStatus: "rejected" }
+                ]
+              }
+            ]
+          },
+          {
+            // THE FIELDS YOU WANT TO RETURN
+            _id: 0,
+            dataSetId: 1, 
+            datasetIds: 1
+          }
+        );
+
+
+
+
+        let tempData = [
+          {
+            "datasetIds": [
+              "7f125091-12ba-464d-af7d-9a88179b0b95",
+              "0cfe60cd-038d-4c03-9a95-894c52135922",
+              // "9e798632-442a-427b-8d0e-456f754d28dc"
+            ]
+          },
+          {
+            "datasetIds": [
+              
+            ],
+            "dataSetId": "0cfe60cd-038d-4c03-9a95-894c52135922"
+          },
+          {
+            "datasetIds": [
+              "9e798632-442a-427b-8d0e-456f754d28dc"
+            ]
+          },
+          {
+            "datasetIds": [
+              "9e798632-442a-427b-8d0e-456f754d28dc"
+            ]
+          }
+        ];
+
+        // console.log(`tempData - ${JSON.stringify(tempData, null, 2)}`)
+
+        // TRY MAP OBJECTS 
+        
+        let thisMap = new Map()
+        console.log(`start thisMap: ${JSON.stringify(thisMap,null,2)}`)
+        console.log(`start thisMap size - ${thisMap.size}`) 
+
+        // tempData.forEach(console.log('item'))
+        for (const x in tempData) {
+          console.log(`DAR - ${JSON.stringify(tempData[x])}`);
+
+          let datasetIdObject = tempData[x];
+
+          if(datasetIdObject.datasetIds && datasetIdObject.datasetIds.length > 0){
+            //ARRAY WITH 1+ VALUE(S)
+
+            datasetIdObject.datasetIds.map((datasetId) => {
+              // console.log(`datasetId: ${datasetId}`)
+              console.log(`type of datasetId: ${typeof datasetId}`)
+              if(thisMap.has(datasetId)){
+               console.log('array string is there')
+              } else {
+                thisMap.set(datasetId, 1)
+              }
+            })
+
+          } 
+          //UPDATE TO LODASH IS NOT EMPTY CHECK?
+          else if(datasetIdObject.dataSetId && datasetIdObject.dataSetId.length > 0){
+              //STRING
+              // console.log(`type of datasetIdObject: ${typeof datasetIdObject}`)
+
+              datasetIdObject = datasetIdObject.toString();
+              console.log(`type of datasetIdObject: ${typeof datasetIdObject}`)
+
+
+              if (thisMap.has(datasetIdObject)){
+               console.log('string is there')
+              } else {
+                // thisMap = {...thisMap, {datasetIdObject, 1}}
+                thisMap.set(datasetIdObject, 1)
+                // console.log(`thisMap: ${JSON.stringify(thisMap, null, 2)}`)
+
+              }
+          }
+          else { console.log('no') }
+        }
+
+        console.log(`thisMap: ${JSON.stringify(thisMap, null, 2)}`)
+        console.log(`thisMap size - ${thisMap.size}`) 
+
+
+        // var q = DataRequestModel.find({ counter: { $gt : 0} }).sort({ counter: -1 }).limit(10);
+
+        // let singleDatasetApplications = await DataRequestModel.find({
+        //   $and: [
+        //     {
+        //       $or: [{ userId: parseInt(userId) }, { authorIds: userId }],
+        //     },
+        //     { dataSetId: { $ne: null } },
+        //   ],
+        // }).populate('dataset mainApplicant');
+
+        
+        datasetIds.exec((err, data) => {
+            if (err) return res.json({ success: false, error: err });
+            return res.json({ success: true, data: data, thisMap: thisMap });
+          });
+
       break;
     }
   });
