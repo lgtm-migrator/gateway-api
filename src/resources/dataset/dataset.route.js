118bb5848c409ca7d9f90391b5cd5753898c6839
Richard <richard.hobbs@paconsulting.com>
2020-11-23 14:09:38 +0000

    Refactoring dataset route to return a single data object instead of a data array

--- src/resources/dataset/dataset.route.js
+++ src/resources/dataset/dataset.route.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { Data } from '../tool/data.model'
 import { loadDataset, loadDatasets } from './dataset.service';
 import { getToolsAdmin } from '../tool/data.repository';
+import _ from 'lodash';
 const router = express.Router();
 const rateLimit = require("express-rate-limit");
 
@@ -59,53 +60,50 @@ router.get(
     }
 );
 
+// @router   GET /api/v1/
+// @desc     Returns a dataset based on either datasetID or PID provided
+// @access   Public
 router.get('/:datasetID', async (req, res) => {
 
-    let datasetId = req.params.datasetID;
+    let { datasetID = ''} = req.params; 
+    if(_.isEmpty(datasetID)) { 
+        return res.status(400).json({ success: false })
+    };
+
     let isLatestVersion = true;
     
     // Search for a dataset based on pid
-    let data = await Data.aggregate([
-        { $match: { $and: [{ pid: datasetId }, {activeflag: 'active'}] } }
-    ]).exec();
-   
-    if(data && data.length > 0){
-        // Set the actual datasetId value based on pid provided
-        datasetId = data[0].datasetid;
+    let dataset = await Data.findOne({ pid: datasetID, activeflag: 'active'}); 
+
+    if(!_.isNil(dataset)){ 
+        // Set the actual datasetId value based on pid provided 
+        datasetID = dataset.datasetid; 
     }
     else{
         // Search for a dataset based on datasetID
-        data = await Data.aggregate([
-            { $match: { $and: [{ datasetid: datasetId }] } }
-        ]).exec();
+        dataset = await Data.findOne({ datasetid: datasetID}); 
         
         // Pull a dataset version from MDC if it doesn't exist on our DB
-        if (data.length === 0) {
-            data[0] = await loadDataset(datasetId)
+        if(_.isNil(dataset)){ 
+            dataset = await loadDataset(datasetID)
         }
 
-        isLatestVersion = (data[0].activeflag === 'active');
+        isLatestVersion = (dataset.activeflag === 'active');
     }
 
-    let pid = data[0].pid;
+    let pid = dataset.pid;
+    let relatedData = await Data.find({ "relatedObjects": { $elemMatch: { "objectId": {$in : [datasetID, pid] } } } });
 
-    let p = Data.aggregate([
-        { $match: {"relatedObjects": { $elemMatch: { "objectId": {$in : [datasetId, pid] } } }} },
-    ]);
-
-    p.exec( async (err, relatedData) => {
-        relatedData.forEach((dat) => {
-            dat.relatedObjects.forEach((x) => {
-                if ((x.objectId === datasetId && dat.id !== datasetId) || (x.objectId === pid && dat.id !== pid)){
-                    if (typeof data[0].relatedObjects === "undefined") data[0].relatedObjects=[];
-                    data[0].relatedObjects.push({ objectId: dat.id, reason: x.reason, objectType: dat.type, user: x.user, updated: x.updated })
-                }
-            })
-        });
-
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true, isLatestVersion: isLatestVersion, data: data });
+    relatedData.forEach((dat) => {
+        dat.relatedObjects.forEach((relatedObject) => {
+            if ((relatedObject.objectId === datasetID && dat.id !== datasetID) || (relatedObject.objectId === pid && dat.id !== pid)){
+                if (typeof dataset.relatedObjects === "undefined") dataset.relatedObjects=[];
+                dataset.relatedObjects.push({ objectId: dat.id, reason: relatedObject.reason, objectType: dat.type, user: relatedObject.user, updated: relatedObject.updated })
+            }
+        })
     });
+
+    return res.json({ success: true, isLatestVersion: isLatestVersion, data: dataset });
 });
 
 // @router   GET /api/v1/
