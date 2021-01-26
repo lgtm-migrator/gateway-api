0c95f89cdb62d19e1bc4b98f463b021b36cf2899
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2021-01-26 15:13:24 +0000

    Removed backward compatibility code causing duplicate DARs in dashboard:

--- src/resources/datarequest/datarequest.controller.js
+++ src/resources/datarequest/datarequest.controller.js
@@ -29,39 +29,21 @@ module.exports = {
 		try {
 			// 1. Deconstruct the
 			let { id: userId } = req.user;
-			// 2. Find all data access request applications created with single dataset version
-			let singleDatasetApplications = await DataRequestModel.find({
-				$and: [
-					{
-						$or: [{ userId: parseInt(userId) }, { authorIds: userId }],
-					},
-					{ dataSetId: { $ne: null } },
-				],
-			}).populate('dataset mainApplicant');
-			// 3. Find all data access request applications created with multi dataset version
-			let multiDatasetApplications = await DataRequestModel.find({
-				$and: [
-					{
-						$or: [{ userId: parseInt(userId) }, { authorIds: userId }],
-					},
-					{
-						$and: [{ datasetIds: { $ne: [] } }, { datasetIds: { $ne: null } }],
-					},
-				],
-			}).populate('datasets mainApplicant');
-			// 4. Return all users applications combined
-			const applications = [...singleDatasetApplications, ...multiDatasetApplications];
 
-			// 5. Append project name and applicants
+			// 2. Find all data access request applications created with multi dataset version
+			let applications = await DataRequestModel.find({ $or: [{ userId: parseInt(userId) }, { authorIds: userId }] }).populate('datasets mainApplicant');
+
+			// 3. Append project name and applicants
 			let modifiedApplications = [...applications]
 				.map(app => {
 					return module.exports.createApplicationDTO(app.toObject(), constants.userTypes.APPLICANT);
 				})
 				.sort((a, b) => b.updatedAt - a.updatedAt);
 
+			// 4. Calculate average decision time across submitted applications
 			let avgDecisionTime = module.exports.calculateAvgDecisionTime(applications);
 
-			// 6. Return payload
+			// 5. Return payload
 			return res.status(200).json({
 				success: true,
 				data: modifiedApplications,
