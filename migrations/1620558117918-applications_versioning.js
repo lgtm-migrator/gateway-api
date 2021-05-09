703415e597582867ab41c5e44914a45506ef0556
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2021-05-09 12:33:15 +0100

    continued up migration scripts

--- migrations/1620558117918-applications_versioning.js
+++ migrations/1620558117918-applications_versioning.js
@@ -1,15 +1,46 @@
-/**
- * Make any changes you need to make to the database here
- */
-async function up () {
-  // Write migration here
+import { DataRequestModel } from '../src/resources/datarequest/datarequest.model';
+
+async function up() {
+
+	// 1. Add default application type to all applications
+	// 2. Add version 1 to all applications
+	// 3. Create version tree for all applications
+
+	// Find all access records
+	let accessRecords = await DataRequestModel.find();
+
+	// Loop through each record
+	for (const accessRecord of accessRecords) {
+
+    accessRecord.applicationType = 'Initial';
+    accessRecord.version = 1;
+    //accessRecord.versionTree = buildVersionTree(accessRecord);
+    
+		await accessRecord.save(async (err, doc) => {
+			if (err) {
+				console.error(`Object update failed for ${accessRecord._id}: ${err.message}`);
+			}
+		});
+	}
 }
 
-/**
- * Make any changes that UNDO the up function side effects here (if possible)
- */
-async function down () {
-  // Write migration here
+async function down() {
+	// Find all access records
+	let accessRecords = await DataRequestModel.find();
+
+	// Loop through each record
+	for (const accessRecord of accessRecords) {
+
+    accessRecord.applicationType = undefined;
+    accessRecord.version = undefined;
+    accessRecord.versionTree = undefined;
+    
+		await accessRecord.save(async (err, doc) => {
+			if (err) {
+				console.error(`Object update failed for ${accessRecord._id}: ${err.message}`);
+			}
+		});
+	}
 }
 
 module.exports = { up, down };
