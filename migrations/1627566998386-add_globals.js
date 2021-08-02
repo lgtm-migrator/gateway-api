2b9f9047878ad132c002f4f863cf0aad28efaf68
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2021-08-02 10:30:17 +0100

    Completed migration script and readme

--- migrations/1627566998386-add_globals.js
+++ migrations/1627566998386-add_globals.js
@@ -29,28 +29,28 @@ const globalData = {
 				definition: 'Allowable dataset usages as per the licencing agreement, following ethical and IG approval',
 				includeInWizard: true,
 				wizardStepTitle: 'Allowable uses',
-				wizardStepDescription: 'What is your type of project? (optional)',
+				wizardStepDescription: 'Please select the identifier that is most relevant to you (optional)',
 				wizardStepOrder: 1,
 				entries: [
 					{
 						displayOrder: 1,
-						definition: 'Available for specific academic research uses only',
 						label: 'Academic research',
 						impliedValues: ['platinum', 'gold', 'silver', 'bronze'],
 					},
 					{
 						displayOrder: 2,
-						definition:
-							'Available for wider commercial uses (in line with ethical and IG approval), and addition to academic and other non-commercial uses',
-						label: 'Commercial',
-						impliedValues: ['platinum', 'gold'],
+						label: 'Student / educational project',
+						impliedValues: ['platinum', 'gold', 'silver', 'bronze'],
 					},
 					{
 						displayOrder: 3,
-						definition:
-							'Available for limited commercial uses (e.g. relating to a specific domain), in addition to academic and other non-commercial uses',
-						label: 'Non-profit',
+						label: 'Charitable or non-profit project',
 						impliedValues: ['platinum', 'gold', 'silver'],
+					},
+					{
+						displayOrder: 4,
+						label: 'Commercial project',
+						impliedValues: ['platinum', 'gold'],
 					},
 				],
 			},
@@ -61,32 +61,33 @@ const globalData = {
 				definition: 'Lag between the data being collected and added to the dataset',
 				includeInWizard: true,
 				wizardStepTitle: 'Time lag',
-				wizardStepDescription: 'What time lag is acceptable? (optional)',
+				wizardStepDescription:
+					'What is the maximum acceptable time delay from the data being generated to entering the dataset? (optional)',
 				wizardStepOrder: 2,
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'Effectively real-time data',
-						label: 'Real-time data',
+						label: 'Near real-time data',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition: 'Approximately 1 week',
 						label: '1 week',
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
-						definition: 'Effectively real-time data',
-						label: 'Approximately 1 month',
+						definition: 'Approximately 1 month',
+						label: '1 month',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
-						definition: 'Effectively real-time data',
-						label: 'Approximately 1 year',
+						definition: 'Approximately 1 year',
+						label: '1 year',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
@@ -97,24 +98,24 @@ const globalData = {
 				definition: 'Average data access request timeframe',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'Less than 2 weeks',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition: 'Less than 1 month',
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
 						definition: 'Less than 3 months',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
 						definition: 'Less than 6 months',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
@@ -125,19 +126,19 @@ const globalData = {
 				definition: 'The level of maturity of the data quality management processes ',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'Externally verified compliance with the data management plan, e.g. by ISO, CQC, ICO or other body',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 2,
 						definition: 'Evidence that the data management plan has been implemented is available',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 3,
 						definition: 'A documented data management plan covering collection, auditing, and management is available for the dataset',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
@@ -148,25 +149,21 @@ const globalData = {
 				definition: 'Representation of multi-disciplinary healthcare data',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'Contains data across more than two tiers',
-						impliedValues: ['platinum', 'gold', 'silver', 'bronze'],
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition: 'Contains multimodal data or data that is linked across two tiers (e.g. primary and secondary care)',
-						impliedValues: ['gold', 'silver', 'bronze'],
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
 						definition: 'Contains data from multiple specialties or services within a single tier of care',
-						impliedValues: ['silver', 'bronze'],
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
 						definition: 'Contains data from a single speciality or area',
 						impliedValues: ['bronze'],
@@ -179,37 +176,33 @@ const globalData = {
 				category: 'Coverage',
 				definition: 'Average timeframe in which a patient appears in a dataset (follow up period)',
 				wizardStepTitle: 'Length of follow up',
-				wizardStepDescription: 'What time frame for patients appearing in datasets is acceptable? (optional)',
+				wizardStepDescription: 'What is the minimum required time frame for patients appearing in dataset, on average? (optional)',
 				includeInWizard: true,
 				wizardStepOrder: 3,
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
-						definition: 'More than 10 years',
-						label: 'More than 10 years',
+						definition: 'Between 1-6 months',
+						label: 'Between 1-6 months',
 						impliedValues: ['platinum', 'gold', 'silver', 'bronze'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
-						definition: 'Between 1-10 years',
-						label: 'Between 1-10 years',
-						impliedValues: ['gold', 'silver', 'bronze'],
-					},
-					{
-						value: 'silver',
-						displayOrder: 3,
 						definition: 'Between 6-12 months',
 						label: 'Between 6-12 months',
-						impliedValues: ['silver', 'bronze'],
+						impliedValues: ['platinum', 'gold', 'silver'],
 					},
 					{
-						value: 'bronze',
+						displayOrder: 3,
+						definition: 'Between 1-10 years',
+						label: 'Between 1-10 years',
+						impliedValues: ['platinum', 'gold'],
+					},
+					{
 						displayOrder: 4,
-						definition: 'Between 1-6 months',
-						label: 'Between 1-6 months',
-						impliedValues: ['bronze'],
+						definition: 'More than 10 years',
+						label: 'More than 10 years',
+						impliedValues: ['platinum'],
 					},
 				],
 			},
@@ -220,25 +213,25 @@ const globalData = {
 				definition: 'Available dataset documentation in addition to the data dictionary',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'As Gold, plus support personnel available to answer questions',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition:
 							'As Silver, plus dataset publication was supported with a journal article explaining the dataset in detail, or dataset training materials',
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
 						definition: 'Comprehensive ReadMe describing extracting and use of data, Dataset FAQS available, Visual data model provided',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
 						definition: 'Past journal articles demonstrate that knowledge of the data exists',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
@@ -249,36 +242,32 @@ const globalData = {
 				definition: 'Availability of clear, documented data model',
 				includeInWizard: true,
 				wizardStepTitle: 'Data model',
-				wizardStepDescription: 'What availability of the data model is acceptable? (optional)',
+				wizardStepDescription: 'What data model requirements do you have? (optional)',
 				wizardStepOrder: 4,
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
-						definition: 'Data Model conforms to a national standard and key fields codified using a national/international standard',
-						label: 'Conforms to national standard and codified to national/international standard',
-            impliedValues: ['platinum', 'gold', 'silver', 'bronze']
-					},
-					{
-						value: 'gold',
-						displayOrder: 2,
-						definition: 'Key fields codified using a national or international standard',
-						label: 'Codified using national/international standard',
-            impliedValues: ['gold', 'silver', 'bronze']
-					},
-					{
-						value: 'silver',
-						displayOrder: 3,
-						definition: 'Key fields codified using a local standard',
-						label: 'Codified using local standard',
-            impliedValues: ['silver', 'bronze']
-					},
-					{
-						value: 'bronze',
-						displayOrder: 4,
 						definition: 'Known and accepted data model but some key field un-coded or free text',
-						label: 'Known and accepted data model ',
-            impliedValues: ['bronze']
+						label: 'Known and accepted data model, with some key fields uncoded ',
+						impliedValues: ['platinum', 'gold', 'silver', 'bronze'],
+					},
+					{
+						displayOrder: 2,
+						definition: 'Key fields codified using a local standard',
+						label: 'Key fields coded using local standard',
+						impliedValues: ['platinum', 'gold', 'silver'],
+					},
+					{
+						displayOrder: 3,
+						definition: 'Key fields codified using a national or international standard',
+						label: 'Key fields coded using national/international standard',
+						impliedValues: ['platinum', 'gold'],
+					},
+					{
+						displayOrder: 4,
+						definition: 'Data Model conforms to a national standard and key fields codified using a national/international standard',
+						label: 'Model conforms to national standard and key fields coded to national/internal standard',
+						impliedValues: ['platinum'],
 					},
 				],
 			},
@@ -289,24 +278,24 @@ const globalData = {
 				definition: 'Provided documented data dictionary and terminologies',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'Dictionary is based on international standards and includes mapping',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition: 'Dictionary relates to national definitions',
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
 						definition: 'Definitions compiled into local data dictionary which is available online',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
 						definition: 'Data definitions available',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
@@ -317,37 +306,33 @@ const globalData = {
 				definition: 'Clear description of source and history of the dataset, providing a "transparent data pipeline"',
 				includeInWizard: true,
 				wizardStepTitle: 'Provenance',
-				wizardStepDescription: 'What availability of the dataset source is acceptable? (optional)',
+				wizardStepDescription: 'To what level of detail do you require the origin of the dataset to be documented? (optional)',
 				wizardStepOrder: 5,
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
-						definition:
-							'Ability to view earlier versions, including versions before any transformations have been applied data (in line with deidentification and IG approval) and review the impact of each stage of data cleaning',
-						label: 'Earlier and ‘raw’ versions and the impact of each stage of data cleaning',
-            impliedValues: ['platinum', 'gold', 'silver', 'bronze']
-					},
-					{
-						value: 'gold',
-						displayOrder: 2,
-						definition: 'All original data items listed, all transformations, rules and exclusion listed and impact of these',
-						label: 'All original data items, transformations, rules, exclusions and impact listed',
-            impliedValues: ['gold', 'silver', 'bronze']
-					},
-					{
-						value: 'silver',
-						displayOrder: 3,
-						definition: 'Source of the dataset and any transformations, rules and exclusions documented',
-						label: 'Dataset source, any transformations, rule and exclusions documented',
-            impliedValues: ['silver', 'bronze']
-					},
-					{
-						value: 'bronze',
-						displayOrder: 4,
 						definition: 'Source of the dataset is documented',
 						label: 'Dataset source documented',
-            impliedValues: ['bronze']
+						impliedValues: ['platinum', 'gold', 'silver', 'bronze'],
+					},
+					{
+						displayOrder: 2,
+						definition: 'Source of the dataset and any transformations, rules and exclusions documented',
+						label: 'Dataset source, any transformations, rule and exclusions documented',
+						impliedValues: ['platinum', 'gold', 'silver'],
+					},
+					{
+						displayOrder: 3,
+						definition: 'All original data items listed, all transformations, rules and exclusion listed and impact of these',
+						label: 'All original data items, transformations, rules, exclusions and impact listed',
+						impliedValues: ['platinum', 'gold'],
+					},
+					{
+						displayOrder: 4,
+						definition:
+							'Ability to view earlier versions, including versions before any transformations have been applied data (in line with deidentification and IG approval) and review the impact of each stage of data cleaning',
+						label: "Earlier and 'raw' versions and the impact of each stage of data cleaning",
+						impliedValues: ['platinum'],
 					},
 				],
 			},
@@ -358,25 +343,25 @@ const globalData = {
 				definition: 'Ability to link with other datasets',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'Existing linkage with reusable or downstream approvals',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition:
 							'List of restrictions on the type of linkages detailed. List of previously successful dataset linkages performed, with navigable links to linked datasets via at DOI/URL',
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
 						definition: 'Available linkages outlined and/or List of datasets previously successfully linked provided',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
 						definition: 'Identifiers to demonstrate ability to link to other datasets',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
@@ -387,24 +372,24 @@ const globalData = {
 				definition: 'Data sources enriched with annotations, image labels, phenomes, derivations, NLP derived data labels',
 				entries: [
 					{
-						value: 'platinum',
 						displayOrder: 1,
 						definition: 'The data include additional derived fields, or enriched data',
+						impliedValues: ['platinum'],
 					},
 					{
-						value: 'gold',
 						displayOrder: 2,
 						definition: 'The data include additional derived fields, or enriched data used by other available data sources',
+						impliedValues: ['gold'],
 					},
 					{
-						value: 'silver',
 						displayOrder: 3,
 						definition: 'The derived fields or enriched data were generated from, or used by, a peer reviewed algorithm',
+						impliedValues: ['silver'],
 					},
 					{
-						value: 'bronze',
 						displayOrder: 4,
 						definition: 'The data includes derived fields or enriched data from a national report',
+						impliedValues: ['bronze'],
 					},
 				],
 			},
