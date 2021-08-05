563236cfcc5e883cc6f5309d986c572308af0a77
Richard <richard.hobbs@paconsulting.com>
2021-08-05 11:36:56 +0100

    Route retrieving cohorts. Cohorts model.  Expanded base repository to  enable searching for a value in a numbers array

---
+++ src/resources/cohort/cohort.model.js
@@ -0,0 +1,35 @@
+import { model, Schema } from 'mongoose';
+
+import CohortClass from './cohort.entity';
+
+const cohortSchema = new Schema(
+	{
+		id: Number,
+		pid: String,
+		description: String,
+		status: String,
+		userId: Number,
+		uploaders: [],
+		isPublic: Boolean,
+		version: Number,
+		changeLog: String,
+		updatedAt: Date,
+
+		// fields from RQuest
+		request_id: String,
+		cohort: {},
+		access_duration: {},
+		items: [],
+		access_duration: {},
+	},
+	{
+		timestamps: true,
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
+
+// Load entity class
+cohortSchema.loadClass(CohortClass);
+
+export const Cohort = model('Cohort', cohortSchema);
