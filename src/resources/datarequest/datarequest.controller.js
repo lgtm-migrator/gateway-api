272f7aac23cef1727c32584b27147e01fb9378f8
Paul McCafferty <>
2021-08-23 17:40:58 +0100

    Fix to file upload

--- src/resources/datarequest/datarequest.controller.js
+++ src/resources/datarequest/datarequest.controller.js
@@ -1180,9 +1180,7 @@ export default class DataRequestController extends Controller {
 			}
 
 			//4. Update all versions of application using version tree
-			await this.dataRequestService.updateFileStatus(accessRecord, fileId).catch(err => {
-				logger.logError(err, logCategory);
-			});
+			await this.dataRequestService.updateFileStatus(accessRecord, fileId);
 
 			return res.status(200).json({
 				success: true,
