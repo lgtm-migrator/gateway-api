7b449db02fcebac59cd5d80cdbe4b3c2a2c638e6
Sam <sam.haim@paconsulting.com>
2021-08-26 09:31:37 +0100

    I added functions declarations in aridhia.service

--- src/resources/dataset/v1/aridhia.service.js
+++ src/resources/dataset/v1/aridhia.service.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+// config
+let config = {};
+config.endpoint = "https://fair.covid-19.aridhia.io/api/datasets/";
+config.token = process.env.ARIDHIA_TOKEN;
+
+// httpService starts here
+const http = axios.create({
+			headers: {
+				Authorization: `Bearer ${config.token}`,
+			},
+			// timeout: 10000
+		});
+
+
+
+function getAllDatasetCodesFromTheAPI();
+function getAridhiaDatasetsFromTheAPI(codes);
+function mapAridhiaDatasetsToDatasetsModels(aridhiaDatasets);
+function updateDB(datasets);
