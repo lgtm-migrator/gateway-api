9211076ba36261d3d165d14fcaf8f5e2385f0560
Paul McCafferty <>
2021-09-07 14:59:40 +0100

    Fix for new issue

--- src/resources/workflow/workflow.repository.js
+++ src/resources/workflow/workflow.repository.js
@@ -52,9 +52,9 @@ export default class WorkflowRepository extends Repository {
 			.lean();
 	}
 
-	async assignWorkflowToApplication(accessRecord, workflowId) {
+	static async assignWorkflowToApplication(accessRecord, workflowId) {
 		// Retrieve workflow using ID from database
-		const workflow = await this.getWorkflowById(workflowId, { lean: false });
+		const workflow = await WorkflowRepository.getWorkflowById(workflowId, { lean: false });
 		if (!workflow) {
 			throw new Error('Workflow could not be found');
 		}
