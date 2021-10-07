d831cd52f3ce24bd68d604e8b39a51c36518888e
Pritesh Bhole <priteshbhole@Priteshs-MacBook-Pro.local>
2021-10-07 12:52:35 +0100

    added tools model

--- migrations/1633525344331-Ig_2354_replace_hubs_with_hub.js
+++ migrations/1633525344331-Ig_2354_replace_hubs_with_hub.js
@@ -1,30 +1,53 @@
-import { firebaseml_v1beta2, toolresults_v1beta3 } from 'googleapis';
 import { PublisherModel } from '../src/resources/publisher/publisher.model';
-
-// import { Data as ToolModel } from '../src/resources/tool/data.model';
+import { Data as ToolModel } from '../src/resources/tool/data.model';
 
 /**
  * Make any changes you need to make to the database here
  */
 async function up () {
+   await toolsUpdate();
+   await publisherUpdate();
+  
+}
+
+async function toolsUpdate() {
+  const tools = await ToolModel.find({ type: "dataset", "datasetfields.publisher": { $regex: "HUBS" } }).lean(); 
+  let tmpTool = [];
+  tools.forEach((tool => {  
+    const { _id } = tool;
+        tmpTool.push({
+          updateOne: {
+            filter: { _id },
+            update: {
+              "datasetfields.publisher": replaceHubs(tool.datasetfields.publisher),
+              "datasetfields.metadataquality.publisher": replaceHubs(tool.datasetfields.metadataquality.publisher),
+              "datasetv2.summary.publisher.memberOf": replaceHubs(tool.datasetv2.summary.publisher.memberOf),
+            }
+          },
+        });
+  }));
+  await ToolModel.bulkWrite(tmpTool);
+}
+
+
+async function publisherUpdate() {
   const publishers = await PublisherModel.find({ "publisherDetails.memberOf": "HUBS" }).lean();
-  let tmp = [];
+  let tmpPub = [];
   publishers.forEach((pub => {
 		const { _id } = pub;
-    const { name } = pub;
-    const { memberOf } = pub.publisherDetails
-    tmp.push({
+    tmpPub.push({
 			updateOne: {
 				filter: { _id },
 				update: {
-          "publisherDetails.memberOf": replaceHubs(memberOf),
-          name : replaceHubs(name),
+          "publisherDetails.memberOf": replaceHubs(pub.publisherDetails.memberOf),
+          "name" : replaceHubs(pub.name),
 				}
 			},
 		});
   }));
-  await PublisherModel.bulkWrite(tmp);
+  await PublisherModel.bulkWrite(tmpPub);
 }
+
 
 function replaceHubs(input) {
  return input.replace('HUBS','HUB')
