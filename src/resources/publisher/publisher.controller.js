e1ab5c5c11b8a3d22df568e04e4d380d454f5e4d
Sam <sam.haim@paconsulting.com>
2021-11-02 10:15:40 +0000

    wip: exporting functions to a service to make it reusable

--- src/resources/publisher/publisher.controller.js
+++ src/resources/publisher/publisher.controller.js
@@ -5,8 +5,12 @@ import { DataRequestModel } from '../datarequest/datarequest.model';
 import { WorkflowModel } from '../workflow/workflow.model';
 import constants from '../utilities/constants.util';
 import teamController from '../team/team.controller';
-
+import { PublisherService } from './publisher.service';
 const datarequestController = require('../datarequest/datarequest.controller');
+
+const getDataRequestsById = async (id) => { 
+
+};
 
 module.exports = {
 	// GET api/v1/publishers/:id
@@ -74,113 +78,18 @@ module.exports = {
 		}
 	},
 
+	// GET api/v1/publishers/team/:team/dataaccessrequests
+	getDataAccessRequestsByPublisherName: async (req, res) => {
+		const publisher = getPublisherById(req.params.publisher); // change the name
+		
+		const publisherService = new PublisherService();
+		return publisherService.getDataRequestsByPublisherId(publisher._id, req.user, res);
+	},
+
 	// GET api/v1/publishers/:id/dataaccessrequests
-	getPublisherDataAccessRequests: async (req, res) => {
-		try {
-			// 1. Deconstruct the request
-			let { _id } = req.user;
-
-			// 2. Lookup publisher team
-			const publisher = await PublisherModel.findOne({ name: req.params.id }).populate('team', 'members').lean();
-			if (!publisher) {
-				return res.status(404).json({ success: false });
-			}
-			// 3. Check the requesting user is a member of the custodian team
-			let found = false;
-			if (_.has(publisher, 'team.members')) {
-				let { members } = publisher.team;
-				found = members.some(el => el.memberid.toString() === _id.toString());
-			}
-
-			if (!found) return res.status(401).json({ status: 'failure', message: 'Unauthorised' });
-
-			//Check if current use is a manager
-			let isManager = teamController.checkTeamPermissions(constants.roleTypes.MANAGER, publisher.team, _id);
-
-			let applicationStatus = ['inProgress'];
-			//If the current user is not a manager then push 'Submitted' into the applicationStatus array
-			if (!isManager) {
-				applicationStatus.push('submitted');
-			}
-			// 4. Find all datasets owned by the publisher (no linkage between DAR and publisher in historic data)
-			let datasetIds = await Data.find({
-				type: 'dataset',
-				'datasetfields.publisher': req.params.id,
-			}).distinct('datasetid');
-			// 5. Find all applications where any datasetId exists
-			let applications = await DataRequestModel.find({
-				$and: [
-					{
-						$or: [{ dataSetId: { $in: datasetIds } }, { datasetIds: { $elemMatch: { $in: datasetIds } } }],
-					},
-					{ applicationStatus: { $nin: applicationStatus } },
-				],
-			})
-				.select('-jsonSchema -questionAnswers -files')
-				.sort({ updatedAt: -1 })
-				.populate([
-					{
-						path: 'datasets dataset mainApplicant',
-					},
-					{
-						path: 'publisherObj',
-						populate: {
-							path: 'team',
-							populate: {
-								path: 'users',
-								select: 'firstname lastname',
-							},
-						},
-					},
-					{
-						path: 'workflow.steps.reviewers',
-						select: 'firstname lastname',
-					},
-				])
-				.lean();
-
-			if (!isManager) {
-				applications = applications.filter(app => {
-					let { workflow = {} } = app;
-					if (_.isEmpty(workflow)) {
-						return app;
-					}
-
-					let { steps = [] } = workflow;
-					if (_.isEmpty(steps)) {
-						return app;
-					}
-
-					let activeStepIndex = _.findIndex(steps, function (step) {
-						return step.active === true;
-					});
-
-					let elapsedSteps = [...steps].slice(0, activeStepIndex + 1);
-					let found = elapsedSteps.some(step => step.reviewers.some(reviewer => reviewer._id.equals(_id)));
-
-					if (found) {
-						return app;
-					}
-				});
-			}
-
-			// 6. Append projectName and applicants
-			let modifiedApplications = [...applications]
-				.map(app => {
-					return datarequestController.createApplicationDTO(app, constants.userTypes.CUSTODIAN, _id.toString());
-				})
-				.sort((a, b) => b.updatedAt - a.updatedAt);
-
-			let avgDecisionTime = datarequestController.calculateAvgDecisionTime(applications);
-			// 7. Return all applications
-			return res.status(200).json({ success: true, data: modifiedApplications, avgDecisionTime, canViewSubmitted: isManager });
-		} catch (err) {
-			console.error(err.message);
-			return res.status(500).json({
-				success: false,
-				message: 'An error occurred searching for custodian applications',
-			});
-		}
+	getDataAccessRequestsByPublisherId: async (req, res) => {
+		const publisherService = new PublisherService();
+		return publisherService.getDataRequestsByPublisherId(req.params.id, req.user, res);
 	},
 
 	// GET api/v1/publishers/:id/workflows
