21f51cd55f13df896315129dec8f70bbb706970f
Dan Nita <dan.nita@hdruk.ac.uk>
2022-06-09 12:40:11 +0300

    try and catch

--- src/services/cachePubSub/cachePubSubClient.js
+++ src/services/cachePubSub/cachePubSubClient.js
@@ -1,16 +1,23 @@
 import { createClient } from 'redis';
-const client = createClient({ 
-    url: process.env.CACHE_URL
-});
 
 export const publishMessageToChannel = async (channel, message) => {
-    if (!client.isOpen) {
-        await client.connect();
+    try {
+        const client = createClient({ 
+            url: process.env.CACHE_URL
+        });
+
+        if (!client.isOpen) {
+            await client.connect();
+        }
+    
+        client.on("connect", () => console.log("Redis cache is ready"));
+        client.on("error", (err) => console.log('Redis Client Error', err));
+        client.on('ready', () => console.log('redis is running'));
+    
+        await client.publish(channel, message);
+    
+    } catch (e) {
+        console.log(e);
+        throw new Error(e.message);
     }
-
-    client.on("connect", () => console.log("Redis cache is ready"));
-    client.on("error", (err) => console.log('Redis Client Error', err));
-    client.on('ready', () => console.log('redis is running'));
-
-    await client.publish(channel, message);
 }
