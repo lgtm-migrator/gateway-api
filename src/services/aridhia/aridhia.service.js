54010030a4d7ce4be5e8f27a661008873c3c40cf
Dan Nita <dan.nita@hdruk.ac.uk>
2022-09-06 18:16:33 +0100

    update

--- src/services/aridhia/aridhia.service.js
+++ src/services/aridhia/aridhia.service.js
@@ -33,7 +33,7 @@ console.log(JSON.stringify(res));
 			description: res.catalogue.description,
 			datasetv2: {},
 			license: res.catalogue.license,
-			name: res.catalog.title,
+			name: res.catalogue.title,
 			publisher: res.catalogue.publisher,
 			rights: res.catalogue.rights,
 			tags: { features: [...res.catalogue.keyword] },
